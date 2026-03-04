9080e8edadc9a9cc32a21aa72741a5543e3e1c33
n-WN <30841158+n-WN@users.noreply.github.com>
2026-03-05 02:06:01 +0800

    bin: configure networking proxy defaults

--- hosts/bin/networking.nix
+++ hosts/bin/networking.nix
@@ -4,5 +4,9 @@
   useDHCP = false;
 
   networkmanager.enable = true;
+  proxy = {
+    default = "http://10.3.50.52:7890";
+    noProxy = "127.0.0.1,localhost,::1,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16";
+  };
   # useNetworkd = true;
 }
