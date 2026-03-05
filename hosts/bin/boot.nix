0eadca64f02428e3f589470322c047f92acca1e1
n-WN <30841158+n-WN@users.noreply.github.com>
2026-03-05 18:53:56 +0800

    boot: add fallback Windows EFI menuentry for GRUB

--- hosts/bin/boot.nix
+++ hosts/bin/boot.nix
@@ -8,6 +8,13 @@
       device = "nodev";
       useOSProber = true;
       efiSupport = true;
+      # Fallback Windows entry in case os-prober is unavailable or skipped.
+      extraEntries = ''
+        menuentry "Windows Boot Manager (EFI)" {
+          search --no-floppy --fs-uuid --set=root F5EF-3E16
+          chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+        }
+      '';
       minegrub-world-sel-theme.enable = true;
     };
     efi.canTouchEfiVariables = true;
