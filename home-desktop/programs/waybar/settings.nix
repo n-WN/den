14e1cf63e0a52af1dca3e025fb393d44ef749752
n-WN <30841158+n-WN@users.noreply.github.com>
2026-03-06 02:57:37 +0800

    waybar: slim down aurora bar height

--- home-desktop/programs/waybar/settings.nix
+++ home-desktop/programs/waybar/settings.nix
@@ -28,11 +28,11 @@ in
     {
       "layer" = "top";
       "position" = "top";
-      "height" = 36;
+      "height" = 32;
       "spacing" = 0;
       "fixed-center" = true;
       "reload_style_on_change" = true;
-      "margin-top" = 2;
+      "margin-top" = 1;
       "margin-left" = 12;
       "margin-right" = 12;
       "modules-left" = selected.waybar.modulesLeft;
