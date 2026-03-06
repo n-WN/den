6e293f212d808b3264dfd1792f618b6216e098aa
n-WN <30841158+n-WN@users.noreply.github.com>
2026-03-07 01:21:02 +0800

    network: update host proxy endpoint to .50

--- hosts/bin/networking.nix
+++ hosts/bin/networking.nix
@@ -5,7 +5,7 @@
 
   networkmanager.enable = true;
   proxy = {
-    default = "http://10.3.50.52:7890";
+    default = "http://10.3.50.50:7890";
     noProxy = "127.0.0.1,localhost,::1,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16";
   };
   # useNetworkd = true;
--- hosts/bin/systemd.nix
+++ hosts/bin/systemd.nix
@@ -1,9 +1,9 @@
 { pkgs, lib }:
 {
   services.nix-daemon.environment = {
-    HTTP_PROXY = "http://10.3.50.52:7890";
-    HTTPS_PROXY = "http://10.3.50.52:7890";
-    ALL_PROXY = "socks5h://10.3.50.52:1080";
+    HTTP_PROXY = "http://10.3.50.50:7890";
+    HTTPS_PROXY = "http://10.3.50.50:7890";
+    ALL_PROXY = "socks5h://10.3.50.50:1080";
     NO_PROXY = "127.0.0.1,localhost,::1";
   };
 
